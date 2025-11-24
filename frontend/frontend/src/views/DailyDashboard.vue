32a367d9214edf03533f1d4c58df868cc93ff81f
MEI ZAHRA <dima@030382.ru>
2025-11-24 07:07:29 +0000

    7

---
+++ frontend/frontend/src/views/DailyDashboard.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="page">
+    <h1>Daily Dashboard</h1>
+    <p>Здесь будет «По дням»</p>
+  </div>
+</template>
---
+++ frontend/frontend/src/views/MonthlyDashboard.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="page">
+    <h1>Monthly Dashboard</h1>
+    <p>Здесь будет «По месяцам»</p>
+  </div>
+</template>
