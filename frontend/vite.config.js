e97c08b42c48c96fac08dad82a10ba419bae7a23
MEI ZAHRA <dima@030382.ru>
2025-11-24 23:50:08 +0300

    Make Vite build use relative base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Относительные ссылки на бандл, чтобы статика подтягивалась корректно
+  // при раздаче из произвольного корня на хостинге.
+  base: './',
 })
